d3afbcbd009c1a1058409f095f9891dcc173bd13
Mazlum <mazlumsert@gmail.com>
2020-01-17 20:56:35 +0100

    Made some more tests

--- tests/unit/Test.php
+++ tests/unit/Test.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Frame;
+use App\Models\Result;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -9,23 +11,78 @@ use PHPUnit\Framework\TestCase;
 class Test extends TestCase
 {
 
+    /**
+     * @var Frame
+     */
+    private $frame;
+    /**
+     * @var Result
+     */
+    private $result;
+
     public function setUp()
     {
+        $this->result = new Result();
+        $this->frame = new  Frame(0, 0);
 
     }
 
     public function testIsStrike()
     {
-        $frame = new \App\Models\Frame(0, 0);
 
-        $this->assertIsBool(false, $frame->isStrike());
+        $this->assertIsBool(false, $this->frame->isStrike());
     }
 
     public function testIsSpare()
     {
-        $frame = new \App\Models\Frame(0, 0);
 
-        $this->assertIsBool(false, $frame->isSpare());
+        $this->assertIsBool(false, $this->frame->isSpare());
+    }
+
+    public function testsetFramesToResult()
+    {
+
+        $frameOne = new Frame(0, 0);
+
+        $frameTwo = new Frame(0, 0);
+
+        $this->result->setFrames([$frameOne, $frameTwo]);
+
+        $this->assertSame(2, count($this->result->getFrames()));
+    }
+
+
+    public function testAddFrameToResult()
+    {
+        $this->result->addFrame(new Frame(0, 0));
+
+        $this->assertSame(1, count($this->result->getFrames()));
+    }
+
+    public function testDeleteFrameToResult()
+    {
+
+        $this->result->addFrame(new Frame(0, 0));
+
+        $this->result->deleteFrame(0);
+
+        $this->assertSame(0, count($this->result->getFrames()));
+    }
+
+    public function testCanGetFrameByResult()
+    {
+
+        $frameObject = new Frame(0, 0);
+
+        $this->result->addFrame($frameObject);
+
+        $this->assertSame(get_class($frameObject), get_class($this->result->getFrame(0)));
+
+    }
+
+    public function testCanGetNullFromResult()
+    {
+        $this->assertNull($this->result->getFrame(0));
     }
 
 
